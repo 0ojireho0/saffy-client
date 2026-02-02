c5ce2d6f793a2a73ecc2b7d0d14f332e0527c38b
0ojireho0 <jeremiahquintano17@gmail.com>
2026-02-03 00:16:27 +0800

    done

--- src/app/(client)/page.jsx
+++ src/app/(client)/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 import Image from "next/image";
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { motion } from "motion/react";
 
 // Icons
@@ -12,28 +12,6 @@ import eccp from "@/assets/images/eccp.jpg"
 import ccap from "@/assets/images/ccap.png"
 
 export default function Home() {
-
-const partnersRef = useRef(null);
-const [inView, setInView] = useState(false);
-
-useEffect(() => {
-  const el = partnersRef.current;
-  if (!el) return;
-
-  const observer = new IntersectionObserver(
-    ([entry]) => {
-      if (entry.isIntersecting) {
-        setInView(true);
-        observer.disconnect(); // animate once
-      }
-    },
-    { threshold: 1 }
-  );
-
-  observer.observe(el);
-  return () => observer.disconnect();
-}, []);
-
 
   // For 3 images
   const items = [
@@ -86,70 +64,12 @@ useEffect(() => {
 
   return (
     <>
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+
 
     {/* 3 Images with hover animation */}
-    <section className="">
+    <section className="min-h-screen">
       <motion.div
-        className="grid grid-cols-1 lg:grid-cols-3"
+        className="grid grid-cols-1 lg:grid-cols-3 min-h-screen"
         variants={container}
         initial="hidden"
         whileInView="show"
@@ -161,7 +81,7 @@ useEffect(() => {
             variants={fadeUp}
             className="
               group relative isolate overflow-hidden bg-slate-950
-              aspect-4/5 cursor-pointer
+              cursor-pointer h-[70vh] lg:h-screen
             "
           >
             {/* Image */}
@@ -201,7 +121,7 @@ useEffect(() => {
                 sailec-medium
               "
             >
-              <h3 className="whitespace-pre-line text-4xl font-semibold leading-tight tracking-tight md:text-5xl">
+              <h3 className="whitespace-pre-line text-4xl leading-tight tracking-tight md:text-5xl">
                 {item.title}
               </h3>
               <p className="mt-2 text-sm text-white/80">{item.author}</p>
@@ -220,7 +140,7 @@ useEffect(() => {
       viewport={{ once: true, amount: 0.4 }}
       transition={{ type: "spring", stiffness: 260, damping: 22 }}
     >
-      <h1 className="text-[#E1F1D5] bg-[#0B2B26] rounded-full p-3 text-sm sailec-medium cursor-pointer hover:scale-105 transition-transform">
+      <h1 className="text-[#E1F1D5] bg-[#0B2B26] rounded-full py-2 px-4 text-sm sailec-medium cursor-pointer hover:scale-105 transition-transform">
         MORE NEWS & STORIES
       </h1>
     </motion.div>
