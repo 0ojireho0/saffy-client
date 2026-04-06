bd9078e7fd3be3c1cab5363ed9b82cf65cd19075
0ojireho0 <jeremiahquintano17@gmail.com>
2026-04-06 18:11:33 +0800

    revert changes

--- src/hooks/auth.js
+++ src/hooks/auth.js
@@ -38,7 +38,6 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
 
     const login = async ({ setErrors, setLoading, ...props }) => {
         await csrf()
-        await axios.get('/session-test');
 
         setErrors([])
         // setStatus(null)
