4e5e7405872870ec4746f6929da1607842982665
0ojireho0 <jeremiahquintano17@gmail.com>
2026-04-06 12:16:29 +0800

    revert

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,19 +1,14 @@
 import Axios from "axios";
 
-export const isProd = process.env.NODE_ENV === "production";
+export const isProd = process.env.NODE_ENV === 'production'
 
 const axios = Axios.create({
-  baseURL: isProd
-    ? process.env.NEXT_PUBLIC_DEPLOYED_BACKEND_API
-    : process.env.NEXT_PUBLIC_BACKEND_API,
-  headers: {
-    "X-Requested-With": "XMLHttpRequest",
-    "Accept": "application/json",
-  },
-  withCredentials: true,
-  withXSRFToken: true,
-  xsrfCookieName: "XSRF-TOKEN",
-  xsrfHeaderName: "X-XSRF-TOKEN",
-});
+    baseURL: isProd ? process.env.NEXT_PUBLIC_DEPLOYED_BACKEND_API : process.env.NEXT_PUBLIC_BACKEND_API,
+    headers: {
+        'X-Requested-With' : 'XMLHttpRequest'
+    },
+    withCredentials: true,
+    withXSRFToken: true
+})
 
-export default axios;
+export default axios
