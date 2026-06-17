591208b3d371d56cffcd5924862d857edc627208
0ojireho0 <jeremiahquintano17@gmail.com>
2026-06-17 08:10:20 +0800

    fix cursor

--- src/app/(client)/layout.js
+++ src/app/(client)/layout.js
@@ -23,7 +23,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bird-cursor`}>
         <Navbar />
         <TooltipProvider>
           {children}
